72e9c3fc7343bd2cdd2445377383081ffd206d11
Winny Silva <winny.silva@yahoo.com.br>
2016-06-07 17:31:50 -0300

    pagina de login funcional

--- Front/Inicial.php
+++ Front/Inicial.php
@@ -2,7 +2,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Login Form</title>
+  <title>Kappa Finances</title>
   <link rel="stylesheet" href="style.css">
   <style>
      body{
@@ -14,16 +14,10 @@
 <body>
   <section class="container">
     <div class="login">
-      <h1>Login to Web App</h1>
-      <form method="post" action="envio.php">
+      <h1>Kappa Finances</h1>
+      <form method="post" action="?go=login ">
         <p><input type="text" name="login" value="" placeholder="CPF"></p>
-        <p><input type="password" name="password" value="" placeholder="Password"></p>
-        <!-- <p class="remember_me">
-          <label>
-            <input type="checkbox" name="remember_me" id="remember_me">
-            Remember me on this computer
-          </label>
-        </p> -->
+        <p><input type="password" name="password" value="" placeholder="Password"></p>    
         <p class="submit"><input type="submit" name="commit" value="Login"></p>
       </form>
     </div>
@@ -32,17 +26,29 @@
       <p>Cadastre-se <a href="Cadastro.php">aqui</a>.</p>
     </div>
   </section>
-<!--
-  <section class="about">
-    <p class="about-links">
-      <a href="http://www.cssflow.com/snippets/login-form" target="_parent">View Article</a>
-      <a href="http://www.cssflow.com/snippets/login-form.zip" target="_parent">Download</a>
-    </p>
-    <p class="about-author">
-      &copy; 2012&ndash;2013 <a href="http://thibaut.me" target="_blank">Thibaut Courouble</a> -
-      <a href="http://www.cssflow.com/mit-license" target="_blank">MIT License</a><br>
-      Original PSD by <a href="http://www.premiumpixels.com/freebies/clean-simple-login-form-psd/" target="_blank">Orman Clark</a>
-  </section>
-    -->
 </body>
 </html>
+<?php
+    include '../Back/usuario.php';
+    session_start();
+    if(@$_GET['go']=="login"){
+    $cpf = $_POST["login"];
+    $senha =  $_POST["password"];    
+    $query = "SELECT cpf,senha FROM usuario WHERE CPF = ".$cpf;    
+    $conn = new AdmDB;
+    $result = $conn->executeQuery($query);
+    $row = mysql_fetch_array($result);
+    $senha2= $row{"senha"};
+    if($senha2==$senha){
+        echo "MATcH";
+        $_SESSION["CPF"] = $cpf;
+        echo $_SESSION["CPF"];
+        header('Location: envio.php');
+        
+    }
+    else{
+         header('Location: Inicial.php');
+    }
+    
+    }
+?>
